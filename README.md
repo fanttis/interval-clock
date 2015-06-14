4304d1e9a03632a1fd4b8fd34ed748812a4b13ee
Antti Huovinen <antti.huovinen@frantic.com>
2015-06-14 19:57:35 +0300

    Js: Update for the new layout and fix some logics.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Interval clock / timer / whatever
 
 http://fanttis.github.io/interval-clock/
+
+## Features
+    Session clock: Time of the whole session.
+    Set clock: Clock for set of intervals. Has log.
+    Interval clock: Time of a single interval. Has log.
+
+## TODO:
+    Add print view
+    Add proper desktop layout
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,9 @@ var Clock = window.Clock || {};
 Clock.main = (function(window, document) {
 
     var time = document.getElementById( 'time' );
-    var tt = document.getElementById( 'tt' );
-    var intervals = document.getElementById( 'intervals' );
+    var rt = document.getElementById( 'rt' );
+    var st = document.getElementById( 'st' );
+    var sprint_log = document.getElementById( 'sprint-log' );
 
     var btn_start_stop = document.getElementById( 'timer-startstop' );
     var btn_interval = document.getElementById( 'timer-interval' );
@@ -25,7 +26,9 @@ Clock.main = (function(window, document) {
         el.className = el.className.replace(cls,' ');
     }
 
-    function pad ( val ) { return val > 9 ? val : "0" + val; }
+    function pad(val){
+        return val > 9 ? val : "0" + val;
+    }
 
     /*
      * #Timer
@@ -36,56 +39,95 @@ Clock.main = (function(window, document) {
 
     var Timer = function(el){
         this.el = el;
+        this.order = 1;
         this.state = null;
         this.clock = null;
         this.Start();
-    }
+    };
 
     Timer.prototype.Start = function(){
         var sec = 0;
         var el = this.el;
+        this.el.getElementsByTagName('span')[0].innerHTML = '00';
+        this.el.getElementsByTagName('span')[1].innerHTML = '00';
         this.state = 1;
 
         this.clock = setInterval( function(){
-            el.getElementsByTagName('time')[0].innerHTML = pad(parseInt(sec/60,10));
-            el.getElementsByTagName('time')[1].innerHTML = pad(++sec%60);
+            el.getElementsByTagName('span')[0].innerHTML = pad(parseInt(sec/60,10));
+            el.getElementsByTagName('span')[1].innerHTML = pad(++sec%60);
         }, 1000);
-    }
+    };
 
     Timer.prototype.Stop = function(){
-        var el = this.el;
         this.state = 0;
         clearInterval(this.clock);
-        el.getElementsByTagName('time')[0].innerHTML = '00';
-        el.getElementsByTagName('time')[1].innerHTML = '00';
-    }
+    };
 
     Timer.prototype.Interval = function(){
-        var log = document.createElement( 'li');
-        log.appendChild( document.createTextNode( this.el.innerText ) );
-        intervals.appendChild(log);
-        this.Stop();
-        this.Start();
+        var log_item = document.createElement( 'li');
+        var el_time = document.createElement( 'time' );
+        var log = this.el.nextSibling.nextSibling;
+
+        el_time.dataset.order = this.order;
+        this.order += 1;
+        log_item.appendChild( el_time ).appendChild( document.createTextNode( this.el.innerText ) );
+        log.insertBefore(log_item, log.firstChild);
+
+        if( this.state === 1 ){
+            this.Stop();
+            this.Start();
+        } else {
+            this.Stop();
+        }
+
+    };
+
+    var runTime;
+    var intervalTime;
+    var sessionTime;
+
+    function saveAndClearSet(){
+        var prev_set = sprint_log.cloneNode(true);
+        prev_set.id = '';
+        prev_set.className = 'log log-history';
+        sprint_log.parentNode.insertBefore( prev_set, sprint_log.nextSibling );
+
+        btn_start_stop.innerHTML = 'Start';
+        sprint_log.innerHTML = '';
+        removeClass( btn_start_stop.parentNode, 'active' );
     }
 
-    var totalTime;
-    var intervalTime;
-
     function startStopTimer(){
-        if( typeof totalTime === 'undefined' || totalTime.state === 0 ){
-            intervalTime = new Timer(time);
-            totalTime = new Timer(tt);
+        if( typeof sessionTime === 'undefined' ){
+            sessionTime = new Timer(st);
+        }
+
+        if( typeof runTime === 'undefined' || runTime.state === 0 ){
+
+            if( typeof runTime === 'undefined' ){
+                intervalTime = new Timer(time);
+                runTime = new Timer(rt);
+            } else {
+                intervalTime.order = 1;
+                intervalTime.Start();
+                runTime.Start();
+            }
+
             btn_start_stop.innerHTML = 'Stop';
+            addClass( btn_start_stop.parentNode, 'active' );
         } else {
-            intervalTime.Stop();
-            totalTime.Stop();
-            btn_start_stop.innerHTML = 'Start';
-            intervals.innerHTML = '';
+            intervalTime.state = 0;
+            intervalTime.Interval();
+
+            runTime.state = 0;
+            runTime.Interval();
+
+            saveAndClearSet();
         }
     }
 
     function addInterval(){
-        if( typeof totalTime !== 'undefined' && totalTime.state === 1 ){
+        if( typeof runTime !== 'undefined' && runTime.state === 1 ){
             intervalTime.Interval();
         }
     }
@@ -98,19 +140,19 @@ Clock.main = (function(window, document) {
         addInterval();
     }, false);
 
-    window.addEventListener( 'keyup', function(e){
-        var k = e.keyCode;
-        switch(k){
+    window.addEventListener( 'keyup', function(event){
+        switch(event.keyCode){
             case 32:
             case 73:
                 // Key: Space, I
-                e.preventDefault();
+                event.preventDefault();
                 addInterval();
                 break;
             case 13:
             case 83:
                 // Key: Enter, S
-                e.preventDefault();
+                if( event.target === btn_start_stop ) return false;
+                event.preventDefault();
                 startStopTimer();
                 break;
             default:
